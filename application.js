92ccf873fc2538892876f1230d6291e844758df5
mutahikagwe <88094318+mutahikagwe@users.noreply.github.com>
2021-07-28 16:12:21 +0300

    training on 28 July

---
+++ application.js
@@ -0,0 +1,18 @@
+/**
+ * This is the application code.
+ *
+ * @author Bernard Nandwa
+ */
+function sayHello() {
+  return 'Hello World, This is my code!';
+}
+
+// call the function
+sayHello();
+
+console.log('Test')
+
+// Show the text on the console
+console.log(sayHello())
+
+

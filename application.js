f200089d3505555ea7965a250089aeacb4a1e280
mutahikagwe <88094318+mutahikagwe@users.noreply.github.com>
2021-07-30 16:18:56 +0300

    variables: introduction to keywords
    - Attached text to html
    - Concatenating strings.

--- application.js
+++ application.js
@@ -4,6 +4,19 @@
  * @author Bernard Nandwa
  */
 function sayHello() {
+  var greeting = 'Goodnight!'
+  greeting = 'Good Morning'
+
+  const greetingStart = "Habari ";
+
+  let salamu;
+  salamu = 'ya mchana'; // Declaration/initializing
+  salamu = 'ya asubuhi'; // reassigning
+
+  document
+    .querySelector('.text')
+    .innerHTML = greetingStart + salamu;
+
   return 'Hello World, This is my code!';
 }
 

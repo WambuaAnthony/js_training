593584fe41fefff1475fd5abc82b9d634065552b
Bernard Nandwa <bernard@nandwa.com>
2021-08-30 15:52:41 +0300

    Conditional statements

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 - Provide weather values for the remaining months
 - Print the weather values in a different color from the month
 - Provide a default value for the inactive months
+
+
--- if.js
+++ if.js
@@ -1,45 +1,89 @@
 let weather;
 
-const month = 'january';
+let month = 'january';
+
+const weatherForMonths = {
+  january: 'sunny',
+  february: 'maybe sunny',
+  march: 'its rainy and sunny'
+};
+
+// use the object to generate select options
+
+const monthSelector = document.querySelector('#selectedMonth');
+
+// monthSelector.addEventListener('change', function() {
+//   month = this.value;
+//
+//   if(month === 'january') {
+//     document
+//       .getElementById('january-weather')
+//       .innerHTML = ' : sunny';
+//   } else if(month === 'february') {
+//     document
+//       .getElementById('february-weather')
+//       .innerHTML = ' : maybe sunny';
+//   }
+// })
+
 
 // || OR
 // && AND
-if(month.length < 4 && month === 'january') {
-  // Print the weather for january on the browser
-  // document
-  //   .getElementById('january-weather')
-  //   .innerHTML = ' : sunny';
+if(month === 'january') {
+  document
+    .getElementById('january-weather')
+    .innerHTML = ' : sunny';
+
+  const januaryWeather = document.getElementById('january')
+    .getElementsByClassName('weather');
+
+  januaryWeather[0].classList.add('bg-red-400','text-white')
 } else if(month === 'february') {
-  // Print the weather for jnuary on the browser
   document
     .getElementById('february-weather')
     .innerHTML = ' : maybe sunny';
-} else if(month === 'may') {
-  // Print the weather for jnuary on the browser
+} else if(month === 'march') {
+  document
+    .getElementById('march-weather')
+    .innerHTML = ' : its rainy and sunny';
+}
+else if(month === 'april') {
+  document
+    .getElementById('april-weather')
+    .innerHTML = ' : its rainy';
+}
+else if(month === 'may') {
   document
     .getElementById('may-weather')
     .innerHTML = ' : maybe rainy';
 } else {
   const divs = document
     .getElementsByClassName('weather');
+
+  console.log(divs)
 }
+
+const divs = document
+  .getElementsByClassName('weather');
+
+console.log(divs)
 
 
 // Switch
-switch (month) {
-  case 'january':
-    document
-      .getElementById('january-weather')
-      .innerHTML = ' : sunny';
-    break;
-  case 'february':
-    document
-      .getElementById('january-weather')
-      .innerHTML = ' : sunny';
-    break;
-  case 'march':
-    document
-      .getElementById('january-weather')
-      .innerHTML = ' : sunny';
-    break;
-}
+// switch (month) {
+//   case 'january':
+//     document
+//       .getElementById('january-weather')
+//       .innerHTML = ' : sunny';
+//     break;
+//   case 'february':
+//     document
+//       .getElementById('january-weather')
+//       .innerHTML = ' : sunny';
+//     break;
+//   case 'march':
+//     document
+//       .getElementById('january-weather')
+//       .innerHTML = ' : sunny';
+//     break;
+// }
